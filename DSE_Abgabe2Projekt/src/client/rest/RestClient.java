5de1f498c1f1adb2ae3e278ab2dce1851dfb8629
Snezana Milutinovic <a1349326@unet.univie.ac.at>
2016-11-26 23:52:39 +0100

    Path eingefuegt

--- DSE_Abgabe2Projekt/src/client/rest/RestClient.java
+++ DSE_Abgabe2Projekt/src/client/rest/RestClient.java
@@ -6,13 +6,14 @@ import org.apache.cxf.jaxrs.client.WebClient;
 public class RestClient implements ClientInterface{
 
 		static final String REST_URI = "http://localhost:9999/";
-	    static final String ADD_PATH = "user/login";
+	    static final String USER_PATH = "user/login";
+	    static final String USER_PATHXML = "user/login/xml";
 	    String s;
 	   
 		    @Override
 			public boolean pruefeLoginXml(String usern, String pwd) {
 		    	WebClient loginClient = WebClient.create(REST_URI);
-		        loginClient.path(ADD_PATH).path(usern + "/" + pwd).accept("text/xml");
+		        loginClient.path(USER_PATHXML).path(usern + "/" + pwd).accept("text/xml");
 		        s = loginClient.get(String.class);
 		        if(s.equals("Willkommen   " + usern)){
 		        System.out.println(s);
@@ -30,7 +31,7 @@ public class RestClient implements ClientInterface{
 			@Override
 			public boolean pruefeLoginPlain(String usern, String pwd) {
 				WebClient loginClient = WebClient.create(REST_URI);
-		        loginClient.path(ADD_PATH).path(usern + "/" + pwd).accept("text/plain");
+		        loginClient.path(USER_PATH).path(usern + "/" + pwd).accept("text/plain");
 		        s = loginClient.get(String.class);
 		        if(s.equals("Willkommen   " + usern)){
 		        System.out.println(s);
--- DSE_Abgabe2Projekt/src/client/rest/testicccc.java
+++ DSE_Abgabe2Projekt/src/client/rest/testicccc.java
@@ -5,7 +5,7 @@ public class testicccc {
 	public static void main(String[] args) {
 		ClientInterface hhhh = new RestClient();
 		
-		hhhh.pruefeLoginXml("gertsch", "hallo");
+		System.out.println(hhhh.pruefeLoginXml("gertsch", "hallo"));
 		hhhh.pruefeLoginPlain("gertsch", "hallo");
 
 	}
