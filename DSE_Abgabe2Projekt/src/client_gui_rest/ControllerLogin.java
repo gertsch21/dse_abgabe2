45827b6058a54cda13a66cf7d3e80a057e67c6c8
Josef Weber <josefweber@jw.local>
2017-01-17 22:17:18 +0100

    Damit der Rest beim Login nicht abkackt wenn der Server offline ist
    -> ein if() mehr

--- DSE_Abgabe2Projekt/src/client_gui_rest/ControllerLogin.java
+++ DSE_Abgabe2Projekt/src/client_gui_rest/ControllerLogin.java
@@ -37,7 +37,7 @@ public class ControllerLogin {
         } else {
             System.out.println("Login attempt: " + unameinput.getText() + " , " + pwinput.getText().toString());
             
-           
+           if (available(9999)){
                 cl = new RestClient();
               
                 Boolean x = cl.pruefeLoginPlain(name, pass);
@@ -64,17 +64,23 @@ public class ControllerLogin {
                     loginerror.setText("Login failed");
                     loginerror.setTextFill(Color.web("#BF0000"));
                 }
-        }
-  
+
+           } else {
+               System.out.println("System: Offline");
+               serverstatus.setText("System: Offline");
+               serverstatus.setTextFill(Color.web("#BF0000"));
+           }
 
         }
 
-    
+    }
 
     public static boolean available(int port) {
-
+        try (Socket ignored = new Socket("localhost", port)) {
             return true;
-
+        } catch (IOException ignored) {
+            return false;
+        }
     }
 
     public void registerClicked() throws IOException {
